632fd14b32b3f498594e2d286dacda9b20fe1f99
Torben <torbenvdh@gmail.com>
2019-04-08 22:16:08 +0200

    Inleveren push 2?

---
+++ Assets/Scripts/Script_Audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Audio : MonoBehaviour
+{
+    AudioSource as_AudioSource;
+    public AudioClip ac_Wind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Audio!");
+        as_AudioSource = GetComponent<AudioSource>();
+        as_AudioSource.PlayOneShot(ac_Wind);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
